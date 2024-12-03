eda93f608b60ddf26c426701aca43298702806d7
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2024-12-03 21:14:04 +0300

    client side initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center'>Assignment10 initialized</h1>
+      <button className="btn btn-secondary">daisyUI checking!!</button>
+    </>
+  )
+}
+
+export default App
